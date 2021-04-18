960c7415eb20276d28a62e16556dc95b23e8868a
krhougs <gh@kt.je>
2021-04-18 04:50:17 +0000

    add polkadotjs_query_reproduce.js

---
+++ src/scripts/polkadotjs_query_reproduce.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env -S node --experimental-json-modules --es-module-specifier-resolution=node --harmony-top-level-await
+import pnp from '../../.pnp.cjs'
+pnp.setup()
+
+import phalaTypes from '@/utils/typedefs'
+import { ApiPromise, WsProvider } from '@polkadot/api'
+
+const phalaProvider = new WsProvider('wss://poc4.phala.network/ws')
+const api = await ApiPromise.create({
+  provider: phalaProvider,
+  types: phalaTypes,
+})
+const hash =
+  '0x85c620607a6cabd43f068f806fe143fc1437126fe0013e28cf967be006f0cea8'
+const val = await api.query.grandpa.currentSetId.at(hash)
+
+console.log(val.toJSON())
