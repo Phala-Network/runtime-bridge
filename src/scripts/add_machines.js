9052bb8de90fc9869abf7e7eef4754aea612b0d4
krhougs <gh@kt.je>
2021-05-07 10:52:03 +0000

    feat: add_machines

---
+++ src/scripts/add_machines.js
@@ -0,0 +1,61 @@
+import { start as startOttoman } from '../utils/couchbase'
+import { getModel } from 'ottoman'
+import { Keyring } from '@polkadot/keyring'
+import { PHALA_SS58_FORMAT } from '../utils/constants'
+import { mnemonicGenerate } from '@polkadot/util-crypto'
+
+const keyring = new Keyring({ type: 'sr25519', ss58Format: PHALA_SS58_FORMAT })
+
+const main = async ({ couchbaseEndpoint, machines }) => {
+  await startOttoman(couchbaseEndpoint)
+  const Machine = getModel('Machine')
+
+  const result = []
+
+  const createMachine = async (machine) => {
+    const mnemonic = mnemonicGenerate()
+    const pair = keyring.createFromUri(mnemonic)
+
+    const { publicKey, address: phalaSs58Address } = pair
+    const polkadotJson = JSON.stringify({
+      publicKey: Array.from(publicKey),
+      phalaSs58Address,
+      mnemonic,
+      pair: pair.toJson(),
+    })
+
+    const m = await Machine.create({
+      nickname: machine.nickname,
+      payoutAddress: machine.payoutAddress,
+      runtimeEndpoint: machine.runtimeEndpoint,
+      phalaSs58Address,
+      polkadotJson,
+    })
+    await m.save()
+    return m
+  }
+
+  for (const machine of machines) {
+    result.push(await createMachine(machine))
+  }
+
+  console.log(JSON.stringify(result))
+}
+
+try {
+  let meta = ''
+  process.stdin.on('readable', () => {
+    const chunk = process.stdin.read()
+    if (chunk) {
+      meta += chunk
+    }
+  })
+
+  process.stdin.on('end', async () => {
+    await main(JSON.parse(meta))
+    process.exit(0)
+  })
+} catch (error) {
+  console.log(error)
+  process.exit(255)
+}
