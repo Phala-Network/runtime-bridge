a1298a28248ad9ae3e5864701fb16caafac30554
Jiacheng Zhang <gh@kt.je>
2021-10-13 15:35:52 +0800

    chore: update polkadot.js

--- src/trade/bus.js
+++ src/trade/bus.js
@@ -1,6 +1,11 @@
-import { TX_QUEUE_SIZE } from '../utils/constants'
+import {
+  TX_BATCH_COMMIT_TIMEOUT,
+  TX_BATCH_SIZE,
+  TX_QUEUE_SIZE,
+} from '../utils/constants'
 import { preprocess } from './preprocess'
 import { setupPhalaApi } from '../utils/api'
+import { v4 as uuid } from 'uuid'
 import createTradeQueue from './trade_queue'
 import env from '../utils/env'
 import logger from '../utils/logger'
@@ -16,23 +21,123 @@ const messageBus = {
   queuesPerPool: {},
 }
 
-const getQueue = (pid) => {
+class PoolQueue {
+  #pid
+  #queue = []
+  #aliveBatches = {}
+  #currentBatch = null
+
+  constructor(pid) {
+    this.#pid = pid
+    this.#dequeueLoop().catch((e) => {})
+  }
+
+  addJob(calls) {
+    const batch = this.#findBatch()
+    batch.addCalls(calls)
+    return batch
+  }
+
+  #findBatch() {
+    if (!this.#currentBatch) {
+      return this.#createBatch()
+    }
+    if (this.#currentBatch.calls.length > TX_BATCH_SIZE) {
+      this.#currentBatch.commit()
+      return this.#createBatch()
+    }
+
+    return this.#currentBatch
+  }
+
+  #createBatch() {
+    let commitTimeout
+    const batchId = uuid()
+    const calls = []
+
+    let startedPromise__resolve, startedPromise__reject
+    const startedPromise = new Promise((resolve, reject) => {
+      startedPromise__resolve = resolve
+      startedPromise__reject = reject
+    })
+
+    let finishedPromise__resolve, finishedPromise__reject
+    const finishedPromise = new Promise((resolve, reject) => {
+      finishedPromise__resolve = resolve
+      finishedPromise__reject = reject
+    })
+
+    const updateCommitTimeout = () => {
+      if (commitTimeout) {
+        clearTimeout(commitTimeout)
+      }
+      commitTimeout = setTimeout(() => commit(), TX_BATCH_COMMIT_TIMEOUT)
+    }
+
+    const wrapError = (fn) => {
+      return (...args) => {
+        try {
+          return fn(...args)
+        } catch (e) {
+          clearTimeout(commitTimeout)
+          throw e
+        }
+      }
+    }
+
+    const addCalls = wrapError((calls) => {
+      updateCommitTimeout()
+      calls.push(...calls)
+    })
+    const commit = wrapError(() => {
+      clearTimeout(commitTimeout)
+      this.#currentBatch = null
+      this.#queue.push(batchRef)
+    })
+
+    const batchRef = {
+      id: batchId,
+      batchId,
+      calls,
+      addCalls,
+      commit,
+      startedPromise,
+      startedPromise__resolve,
+      startedPromise__reject,
+      finishedPromise,
+      finishedPromise__resolve,
+      finishedPromise__reject,
+    }
+
+    this.#aliveBatches[batchId] = batchRef
+
+    return batchRef
+  }
+
+  get pid() {
+    return this.#pid
+  }
+
+  get queue() {
+    return this.#queue
+  }
+
+  async #dequeueLoop() {}
+
+  get currentBatch() {
+    return this.#currentBatch
+  }
+}
+
+const getPoolQueue = (pid) => {
   const pidStr = `${pid}`
   let queue = messageBus.queuesPerPool[pidStr]
   if (queue) {
     return queue
   }
-  queue = createPoolQueue(pidStr)
+  queue = new PoolQueue(pidStr)
   messageBus.queuesPerPool[pidStr] = queue
   return queue
-}
-
-const createPoolQueue = (pid) => {
-  const queue = []
-  return {
-    pid,
-    queue,
-  }
 }
 
 const startMessageBus = async (appContext) => {
@@ -45,8 +150,22 @@ const startMessageBus = async (appContext) => {
 
   logger.info('Now accepting incoming transaction requests...')
 
+  const callRefs = {}
+
   const enqueue = async (callMeta) => {
-    throw new Error('todo')
+    const poolQueue = getPoolQueue(callMeta.pid)
+    const batch = poolQueue.addJob(callMeta.calls)
+    callRefs[callMeta.id] = batch
+    await batch.startedPromise
+    return async () => {
+      try {
+        await batch.finishedPromise
+        delete callRefs[callMeta.id]
+      } catch (e) {
+        delete callRefs[callMeta.id]
+        throw e
+      }
+    }
   }
 
   inputQueue.process(TX_QUEUE_SIZE, async (job) => {
@@ -61,7 +180,7 @@ const startMessageBus = async (appContext) => {
       await waitUntilFinished()
       await job.reportProgress(RP_FINISHED)
     }
-    const rescureJob = (e) => {
+    const rescueJob = (e) => {
       if (!remainingRetries) {
         job.reportProgress(RP_FAILED)
         throw e
@@ -69,9 +188,9 @@ const startMessageBus = async (appContext) => {
       job.reportProgress(RP_SHOULD_RETRY)
       remainingRetries -= 1
       logger.warn(`Retrying job #${job.id} with error:`, e)
-      return processJob().catch(rescureJob)
+      return processJob().catch(rescueJob)
     }
-    await processJob().catch(rescureJob)
+    await processJob().catch(rescueJob)
   })
 
   Object.assign(appContext, { messageBus })
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -22,8 +22,10 @@ export const APP_MESSAGE_TUNNEL_QUERY_TIMEOUT = 15000
 
 export const FRNK = '0x46524e4b'
 
-export const TX_QUEUE_SIZE = 320
-export const TX_SEND_QUEUE_SIZE = 50
+export const TX_QUEUE_SIZE = 1000
+export const TX_BATCH_SIZE = 100
+export const TX_SEND_QUEUE_SIZE = 10
+export const TX_BATCH_COMMIT_TIMEOUT = 12000
 export const TX_TIMEOUT = 8 * 60 * 60 * 1000 // TODO: temp fix, remove later!
 
 export const BLOB_MAX_RANGE_COUNT = 900
