dcaa22f627d35181bbd7970446dd79cc807d645e
krhougs <os@kt.je>
2023-01-29 14:52:45 +0800

    lifecycle: extend default value of blobRequestTimeout to 150s

--- src/lifecycle/env.ts
+++ src/lifecycle/env.ts
@@ -74,7 +74,7 @@ export const debugAllowBlobFromSynchingState =
   env.debugAllowBlobFromSynchingState === 'true'
 
 export const rpcRequestTimeout = parseInt(env.rpcRequestTimeout) || 8000
-export const blobRequestTimeout = parseInt(env.blobRequestTimeout) || 60000
+export const blobRequestTimeout = parseInt(env.blobRequestTimeout) || 150000
 export const syncOnly = env.syncOnly === 'true'
 
 export const prpcQueueSize = parseInt(env.prpcQueueSize) || 65535
